ee9277384e0c94790f6681d4818a131dc78be269
Benjamin Hohenwarter <bh@vivid-planet.com>
2014-12-17 10:10:43 +0100

    Initial commit, creating first script
    This first script is used to convert Koala Framework trl.xml into
    standard trl.po file for use with standard-tools.

---
+++ Kwf/Trl/ConvertTrlXmlToPoCommand.php
@@ -0,0 +1,37 @@
+<?php
+namespace Kwf\Trl;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ConvertTrlXmlToPoCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('convertTrlXmlToPo')
+            ->setDescription('Convert xml-trl file into po-trl file')
+            ->addArgument('xmlPath', InputArgument::REQUIRED, 'Path to trl xml file')
+            ->addArgument('poPath', InputArgument::OPTIONAL, 'Filename for trl po file', 'trl.po')
+            ->addArgument('baseLanguage', InputArgument::OPTIONAL, 'Language used as id (default is en)', 'en')
+            ->addArgument('targetLanguage', InputArgument::OPTIONAL, 'Language used for translation (default is none)');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $xmlPath = $input->getArgument('xmlPath');
+        $poPath = $input->getArgument('poPath');
+        $baseLanguage = $input->getArgument('baseLanguage');
+        $targetLanguage = $input->getArgument('targetLanguage');
+
+        $trlXmlToPoConverter = new TrlXmlToPoConverter();
+        $trlXmlToPoConverter->setXmlPath($xmlPath);
+        $trlXmlToPoConverter->setBaseLanguage($baseLanguage);
+        if ($targetLanguage) $trlXmlToPoConverter->setTargetLanguage($targetLanguage);
+        $trlXmlToPoConverter->convertToPo();
+
+        $trlXmlToPoConverter->writePoContent($poPath);
+        exit();
+    }
+}
---
+++ Kwf/Trl/TrlXmlToPoConverter.php
@@ -0,0 +1,76 @@
+<?php
+namespace Kwf\Trl;
+
+class TrlXmlToPoConverter
+{
+    protected $_xmlDocument;
+    protected $_poContent;
+    protected $_baseLanguage;
+    protected $_targetLanguage;
+
+    //https://www.gnu.org/software/gettext/manual/html_node/PO-Files.html
+    public function __construct()
+    {
+    }
+
+    public function setBaseLanguage($language)
+    {
+        $this->_baseLanguage = $language;
+    }
+    public function setTargetLanguage($language)
+    {
+        $this->_targetLanguage = $language;
+    }
+    public function setXmlPath($xmlPath)
+    {
+        $this->setXmlContent(file_get_contents($xmlPath));
+    }
+    public function setXmlContent($content) // needed for testing
+    {
+        $this->_xmlDocument = simplexml_load_string($content);
+    }
+
+    public function convertToPo()
+    {
+        $this->_poContent = array();
+        foreach ($this->_xmlDocument->text as $trl) {
+            if ($trl->context) {
+                $this->_poContent[] = 'msgctxt "'.$trl->context.'"';
+            }
+            if ($trl->{$this->_baseLanguage}) {
+                $this->_poContent[] = 'msgid "'.$this->_escapeString($trl->{$this->_baseLanguage}).'"';
+            }
+            if ($trl->{$this->_baseLanguage.'_plural'}) {
+                $this->_poContent[] = 'msgid_plural "'.$this->_escapeString($trl->{$this->_baseLanguage.'_plural'}).'"';
+                if ($this->_targetLanguage) {
+                    $this->_poContent[] = 'msgstr[0] "'.$this->_escapeString($trl->{$this->_targetLanguage}).'"';
+                    $this->_poContent[] = 'msgstr[1] "'.$this->_escapeString($trl->{$this->_targetLanguage.'_plural'}).'"';
+                } else {
+                    $this->_poContent[] = 'msgstr[0] ""';
+                    $this->_poContent[] = 'msgstr[1] ""';
+                }
+            } else {
+                if ($this->_targetLanguage) {
+                    $this->_poContent[] = 'msgstr "'.$this->_escapeString($trl->{$this->_targetLanguage}).'"';
+                } else {
+                    $this->_poContent[] = 'msgstr ""';
+                }
+            }
+            $this->_poContent[] = '';
+        }
+    }
+
+    private function _escapeString($string)
+    {
+        return str_replace("\'", "'", str_replace('"', '\"', $string));
+    }
+
+    public function getPoContent() // needed for testing
+    {
+        return $this->_poContent;
+    }
+    public function writePoContent($poPath)
+    {
+        return file_put_contents($poPath, implode("\n", $this->_poContent));
+    }
+}
