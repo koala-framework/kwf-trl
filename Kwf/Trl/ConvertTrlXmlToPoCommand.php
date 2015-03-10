72ffc4dbf23fdd7510efd3a4a913015227551a11
Benjamin Hohenwarter <bh@vivid-planet.com>
2015-03-10 11:08:39 +0100

    Use options instead of arguments

--- Kwf/Trl/ConvertTrlXmlToPoCommand.php
+++ Kwf/Trl/ConvertTrlXmlToPoCommand.php
@@ -3,6 +3,7 @@ namespace Kwf\Trl;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Kwf\Trl\Convert\TrlXmlToPoConverter;
@@ -14,17 +15,17 @@ class ConvertTrlXmlToPoCommand extends Command
         $this->setName('convertTrlXmlToPo')
             ->setDescription('Convert xml-trl file into po-trl file')
             ->addArgument('xmlPath', InputArgument::REQUIRED, 'Path to trl xml file')
-            ->addArgument('poPath', InputArgument::OPTIONAL, 'Filename for trl po file', 'trl.po')
-            ->addArgument('baseLanguage', InputArgument::OPTIONAL, 'Language used as id (default is en)', 'en')
-            ->addArgument('targetLanguage', InputArgument::OPTIONAL, 'Language used for translation (default is none)');
+            ->addOption('poPath', 'p', InputOption::VALUE_OPTIONAL, 'Filename for trl po file', 'trl.po')
+            ->addOption('baseLanguage', 'b', InputOption::VALUE_OPTIONAL, 'Language used as id (default is en)', 'en')
+            ->addOption('targetLanguage', 't', InputOption::VALUE_OPTIONAL, 'Language used for translation (default is none)');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $xmlPath = $input->getArgument('xmlPath');
-        $poPath = $input->getArgument('poPath');
-        $baseLanguage = $input->getArgument('baseLanguage');
-        $targetLanguage = $input->getArgument('targetLanguage');
+        $poPath = $input->getOption('poPath');
+        $baseLanguage = $input->getOption('baseLanguage');
+        $targetLanguage = $input->getOption('targetLanguage');
 
         $trlXmlToPoConverter = new TrlXmlToPoConverter();
         $trlXmlToPoConverter->setXmlPath($xmlPath);
--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 This project consists of two scripts:
 
-## Convert: ##
-
-This is used to convert an existing *trl.xml* into the more general *trl.po*.
-
-### Usage: ###
-php bin/trl convertTrlXmlToPo PATH-TO-XML PATH-FOR-GENEREATED-PO-FILE LANGUAGE-TO-USE-AS-ID LANGUAGE-OF-TRANSLATION
-
-
 ## Parse: ##
 
 This script parses a complete directory/package/web (.js, .php, .tpl) for trl() or trlKwf() function calls and generates a .po-file.
@@ -15,4 +7,12 @@ If used for package or web define kwf-path to exclude trls covered by kwf
 
 
 ### Usage: ###
-php bin/trl parseCode PATH-FOR-GENERATED-PO-FILE MASK(trl/trlKwf) DIRECTORY-TO-PARSE PATH-TO-KWF
+`php bin/trl parseCode [-p|--path="..."] [-m|--mask="..."] [-k|--kwfpath="..."] [dir]`
+
+
+## Convert: ##
+
+This is used to convert an existing *trl.xml* into the more general *trl.po*.
+
+### Usage: ###
+`php bin/trl convertTrlXmlToPo [-p|--poPath[="..."]] [-b|--baseLanguage[="..."]] [-t|--targetLanguage[="..."]] xmlPath`
