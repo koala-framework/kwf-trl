301d804ecee807f32d73484a7164cf89fec213b7
Georg Schreglmann <georg.schreglmann@vivid-planet.com>
2021-12-22 15:49:50 +0100

    Add branches as option for parseWeb

--- Kwf/Trl/ParseWebCommand.php
+++ Kwf/Trl/ParseWebCommand.php
@@ -5,24 +5,27 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Kwf\Trl\ParseGitBranches;
+use Symfony\Component\Console\Input\InputOption;
 
 class ParseWebCommand extends Command
 {
     protected function configure()
     {
         $this->setName('parseWeb')
-            ->setDescription('Parse your web code for trl function calls');
+            ->setDescription('Parse your web code for trl function calls')
+            ->addOption('branches', null, InputOption::VALUE_OPTIONAL, 'Branches of repository you want to parse');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $branches = $input->getOption('branches') ? explode(',', $input->getOption('branches')) : null;
         $config = parse_ini_file('config.ini');
         $webcodeLanguage = $config['webCodeLanguage'];
         $poFilePath = "trl/$webcodeLanguage.po";
         if (!is_dir('trl')) {
             mkdir('trl');
         }
-        $parseScript = new ParseGitBranches(getcwd(), $poFilePath, 'web', $output);
+        $parseScript = new ParseGitBranches(getcwd(), $poFilePath, 'web', $output, false, $branches);
         $parseScript->parse();
     }
 }
